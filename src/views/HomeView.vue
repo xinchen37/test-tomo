effd8772b199cc02bd821e2429872a14113c7f3d
qiang xiao <xiaoqiang@citik.xyz>
2025-05-20 18:58:40 +0800

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { getWalletState, initTomoModal } from '@tomo-inc/tomo-web-sdk'
+import '@tomo-inc/tomo-web-sdk/style.css'
+
+import { onMounted } from 'vue'
+
+const tomoModal = () => {
+  initTomoModal({
+    theme: 'light',
+    clientId:
+      'izEkhMZuzamb07eI3Vv7lscUK6BmoOG6xtOKFuodirUl6xuL57PPl0TolUBIzJN5fpgBsmtMpSpwQ0krBEtE71Bm',
+    chainTypes: ['evm'],
+    onConnect: () => {
+      const walletState = getWalletState()
+      console.log('onConnect', walletState)
+    },
+  })
+}
+
+const openModal = () => {
+  window.openTomoConnectModal?.()
+}
+
+onMounted(() => {
+  tomoModal()
+})
+</script>
+
+<template>
+  <main>
+    <div class="text-div" @click="openModal()">@tomo-inc/tomo-web-sdk</div>
+  </main>
+</template>
+
+<style lang="css">
+.text-div {
+  font-size: 30px;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
